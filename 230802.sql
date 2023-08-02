78f58711e4a7292e72c77d19f8c505833aca0b2d
hoonj170214 <sh20170214@gmail.com>
2023-08-02 11:47:30 +0900

    self join, outer join

--- 230802.sql
+++ 230802.sql
@@ -61,7 +61,8 @@ select * from emp, dept WHERE emp.deptno = dept.deptno;
 	WHERE 검색조건
     
     ON, WHERE 뒤에 조건을 쓸때 '테이블.열 이름' 형식으로 써줘야 한다.
-    
+    WHERE 검색조건은 호환성 등의 문제로 권장하지 않는 방법.
+    ON 검색조건을 애용하자.
 */
 SELECT * FROM emp INNER JOIN dept;
 SELECT * FROM emp INNER JOIN dept
@@ -101,3 +102,74 @@ SELECT deptno FROM dept WHERE loc = 'NEW YORK'
 -- 내추럴 조인을 이용해서 검색(NATURAL JOIN)
 SELECT emp.ename, emp.sal FROM emp NATURAL JOIN dept
 WHERE dept.loc = 'NEW YORK';
+
+-- SCOTT이 근무하는 부서의 이름과 SCOTT 사원의 급여 출력
+-- where과 and를 사용해서 검색
+select dept.dname, emp.sal from emp,dept
+where emp.deptno = dept.deptno and emp.ename = 'SCOTT';
+
+-- INNER JOIN, USING을 사용해서 검색 
+select dname, sal from emp JOIN dept USING(deptno)
+where emp.ename = 'SCOTT'; 
+
+-- SELF JOIN 
+-- 동일한 테이블을 결합하여 검색
+select * from emp;
+-- 사원의 이름과 그 사원의 매니저 이름을 검색
+-- 같은 테이블을 조인할때는 AS로 테이블의 별칭을 지정해줘야 한다.
+select * from emp AS A, emp AS B; -- 에러 : 별칭을 지정하지 않으면 에러 뜸.
+
+-- emp와 mgr가 일치하는 데이터만 출력
+select * from emp AS A, emp AS B
+WHERE A.mgr = B.empno;
+
+select A.ename AS 사원이름 , B.ename AS 매니저이름
+from emp AS A, emp AS B
+WHERE A.mgr = B.empno;
+
+/*
+	- OUTER JOIN
+    - JOIN 조건에 만족하지 않는 행 정보도 남아있는 테이블의 값이 존재하면 
+    - 검색에 포함
+    - 조인의 조건에 만족되지 않는 행까지도 포함시키는 것
+    
+    - 책
+    SELECT 열목록
+    FROM 첫번째 테이블(left 테이블)
+    <left | RIGHT | FULL> OUTER JOIN 두번째 테이블(right 테이블)
+		ON 검색될 조건
+	[WHERE 검색조건];
+    
+    - LEFT, RIGHT 를 작성하면 OUTER는 안적어도 됨
+*/
+
+-- 사원의 이름과 매니저 이름을 출력하되 매니저가 없는 사원 정보도 검색
+SELECT A.ename AS 사원, B.ename AS 매니저
+FROM emp AS A LEFT OUTER JOIN emp AS B
+ON A.mgr = B.empno;
+
+-- 검색된 결과를 이름을 기준으로 정렬
+SELECT A.ename AS 사원, B.ename AS 매니저
+FROM emp AS A LEFT OUTER JOIN emp AS B
+ON A.mgr = B.empno ORDER BY B.ename;
+
+-- 사원의 이름과 매니저 이름을 출력하되 매니저가 아닌 사원 정보도 출력
+SELECT A.ename AS 사원, B.ename AS 매니저
+FROM emp AS A RIGHT OUTER JOIN emp AS B
+ON A.mgr = B.empno ORDER BY B.ename;
+
+-- emp table과 salgrade table을 조인하여
+-- 각 사원의 급여등급을 사원명, 급여, 급여 등급으로 출력
+
+select * from salgrade;
+select * from emp e LEFT JOIN salgrade s
+ON e.sal between s.losal and s.hisal;
+
+/*
+	--JOIN
+    -- 각 사원의 급여 등급을 사원명, 부서명, 급여, 급여 등급으로 검색
+    
+*/
+
+
+
